bee5fe088a762fc659981769969de7680eaee541
tung <nguyenthanhtungn5@gmail.com>
2025-08-10 16:00:28 +0200

    DB: Fix wrong input table

--- db/scripts/02_seed.sql
+++ db/scripts/02_seed.sql
@@ -1,7 +1,8 @@
-INSERT INTO roles (name) VALUES ('admin'), ('user'), ('support');
+INSERT INTO roles(name) VALUES ('admin'), ('user'), ('support');
 
-INSERT INTO permissions (name) VALUES
+INSERT INTO permissions(name) VALUES
   ('user_read'), ('user_create'), ('user_delete'), ('role_read'), ('role_create'), ('role_delete');
 
-INSERT INTO users (first_name, last_name, email, phone) VALUES
-  ('Tung'), ('Nguyen'), ('tung@gmail.com'), ('01234556789');
+INSERT INTO users(first_name, last_name, email, phone) VALUES
+  ('Tung', 'Nguyen', 'default@gmail.com', '01234556789') ON 
+  CONFLICT (email) DO NOTHING;
