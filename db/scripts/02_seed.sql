62def269b241e538c86d71aa5b081fc72e5504cc
tung <nguyenthanhtungn5@gmail.com>
2025-08-26 00:04:31 +0200

    DB: Update seed

--- db/scripts/02_seed.sql
+++ db/scripts/02_seed.sql
@@ -4,5 +4,10 @@ INSERT INTO permissions(name) VALUES
   ('user_read'), ('user_create'), ('user_delete'), ('role_read'), ('role_create'), ('role_delete');
 
 INSERT INTO users(first_name, last_name, email, phone) VALUES
-  ('Tung', 'Nguyen', 'default@gmail.com', '01234556789') ON 
-  CONFLICT (email) DO NOTHING;
+  ('Tung', 'Nguyen', 'default@gmail.com', '01234556789');
+
+INSERT INTO role_permissions(role_id, permission_id) VALUES
+  (1, 1);
+
+INSERT INTO user_roles(user_id, role_id) VALUES
+  (1, 1);
