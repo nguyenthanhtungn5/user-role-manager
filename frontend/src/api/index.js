25f9e86a89be3a333b91d437698e3341f5d5d7ed
tung <nguyenthanhtungn5@gmail.com>
2026-03-12 22:52:52 +0100

    Use default port

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:
-    import.meta?.env?.VITE_API_BASE || process.env.VUE_APP_API_BASE || 'http://localhost:8888',
+  baseURL: import.meta?.env?.VITE_API_BASE || process.env.VUE_APP_API_BASE || '/api',
 })
 
 export function getAllUsers() {
